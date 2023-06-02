24ec37534d1d366eda488ac9c19b010366d35a19
Calvin <67180946+deleteme0@users.noreply.github.com>
2023-06-02 21:22:22 +0530

    changed api url

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://fso-part3-gzzr.onrender.com/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
